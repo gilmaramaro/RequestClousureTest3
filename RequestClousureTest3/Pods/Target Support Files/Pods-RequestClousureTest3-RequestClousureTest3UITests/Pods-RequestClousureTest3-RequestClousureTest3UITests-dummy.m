443d447e731854614cd0d9fe20dfbe7d194e1487
Gilmar Amaro <gilmar.amaro13@gmail.com>
2023-03-27 16:01:09 -0300

    Project Created

---
+++ RequestClousureTest3/Pods/Target Support Files/Pods-RequestClousureTest3-RequestClousureTest3UITests/Pods-RequestClousureTest3-RequestClousureTest3UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RequestClousureTest3_RequestClousureTest3UITests : NSObject
+@end
+@implementation PodsDummy_Pods_RequestClousureTest3_RequestClousureTest3UITests
+@end
---
+++ RequestClousureTest3/RequestClousureTest3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RequestClousureTest3
+//
+//  Created by Gilmar Amaro on 27/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
